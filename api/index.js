b338e3cd7d8c1b0655c762c1939df5c51efbcf91
sophiallen <smlelevich@gmail.com>
2017-05-04 13:59:22 -0700

    Added webcrawler, using promises.

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 import express from 'express';
-const router = express.Router();
-import request from 'request';
-import cheerio from 'cheerio';
-import Promise from 'bluebird';
+import crawler from '../lib/WebCrawl.js';
 
-Promise.promisifyAll(request);
+const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.send({data: []});
@@ -14,49 +11,9 @@ router.get('/search', (req, res) => {
 	console.log("query: " + req.query.jobTitle + " " + req.query.city);
 	let jobTitle = req.query.jobTitle;
 	let city = req.query.city;
-	let qs  = '/search/jjj?query=' + jobTitle + '&sort=rel';
-	let baseUrl = 'https://' + req.query.city + '.craigslist.org';
-
-	request( baseUrl + qs,
-	 (err, response, body) => {
-		  // console.log('error:', err); // Print the error if one occurred 
-		  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-		  const doc = cheerio.load(body);
-
-		  let links = doc('a.result-title');
-
-		  let refs = [];
-
-		  for (let i = 0; i < 10; i++){
-		  	refs.push(links[i].attribs.href);
-		  }
-
-		  let bods = Promise.resolve();
-
-		  Promise.map(refs, (link) => {
-		  	  console.log("processing " + link);
-		  	  bods = bods.then(function() {
-				  return request.getAsync(baseUrl + link);
-		  	  });
-		  	  return bods;
-		  }).map(function(respBody){
-		  	console.log("getting respbody");
-		  	return respBody[1];
-		  }).then(function() {
-		  		console.log("all requests saved");
-		  		// console.log(bods);
-		  }).catch(function(e){
-		  		console.log("error: " + e);
-		  });
-
-		  // let bods = refs.map((link) =.t> {
-			 //  request(baseUrl + link, (err, resp, bod) => {
-			 //  		console.log("got one, " + bod.length);
-			 //  		return bod;
-			 //  });		  	
-		  // });
-
-		  res.send("done");
+	
+	crawler.search(city, jobTitle).then((result) => {
+		res.send(result);
 	});
 });
 
